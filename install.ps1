191bf241286031de3fbb4b054244de95c621a0a2
redhcp <lerner20037@gmail.com>
2022-10-16 23:45:44 +0300

    add scrp install

--- install.ps1
+++ install.ps1
@@ -5,10 +5,11 @@ cd $env:HOMEPATH\Downloads
 Get-ChildItem dotfiles-main.zip | Expand-Archive 
 #install fonts
 cd .\dotfiles-main\dotfiles-main\fonts
-Get-ChildItem hack.zip | Expand-Archive -Verbose
-Get-ChildItem mononoki.zip | Expand-Archive -Verbose
-Copy-Item .\hack\hack\* "C:\Windows\Fonts\" -Force -Verbose
-cd $env:USERPROFILE\Downloads\dotfiles-main\dotfiles-main\fonts\
-Copy-Item .\mononoki\mononoki\* "C:\Windows\Fonts\" -Force -Verbose 
+Get-ChildItem hack.zip | Expand-Archive -Force -Verbose
+Get-ChildItem mononoki.zip | Expand-Archive -Force -Verbose
+Get-ChildItem anonymouspro.zip | Expand-Archive -Verbose
+Copy-Item -Path "$env:USERPROFILE\Downloads\dotfiles-main\dotfiles-main\fonts\mononoki\mononoki\*" "C:\Windows\Fonts\" -Force -Verbose
+Copy-Item -Path "$env:USERPROFILE\Downloads\dotfiles-main\dotfiles-main\fonts\hack\hack\*" "C:\Windows\Fonts\" -Force -Verbose
+Copy-Item -Path "$env:USERPROFILE\Downloads\dotfiles-main\dotfiles-main\fonts\anonymouspro\anonymouspro\*" "C:\Windows\Fonts\" -Force -Verbose
 #install wallpapers
-Copy-Item -path "$env:USERPROFILE\Downloads\dotfiles-main\dotfiles-main\wallpapers\*" -Destination "$env:USERPROFILE\Pictures\Saved Pictures\"  -Force
+Copy-Item -path "$env:USERPROFILE\Downloads\dotfiles-main\dotfiles-main\wallpapers\*" -Destination "$env:USERPROFILE\Pictures\Saved Pictures\" -Force -Verbose
